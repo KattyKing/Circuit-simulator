68070ca60f8d82389aedb0cd0f2091c968f7e71b
KattyKing <38794317+KattyKing@users.noreply.github.com>
2018-06-11 21:26:32 +0200

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -1,25 +1,26 @@
 RESISTORS = []
 
 class Resistor:
-	'''Class 'Resistors'. Create the object resistor and assign a resistance.'''
+	'''Class for creating resistor objects and calculating its voltage.'''
 	def __init__(self, resistance):
+		'''Method sets initial parameter resistance.'''
 		self.resistance = resistance
 		
 	def voltage_on_resistor(self, I):
-		'''Method voltage_on_resistor. Calculate voltage on resistor.'''
+		'''Method takes total current and calculate voltage on resistor.'''
 		self.voltage_on_res = self.resistance * I
 		return self.voltage_on_res
 		
 
 class Source:
-	'''Class 'Source' just create source object and assign a voltage value. '''
+	'''Class for creating source objects. '''
 	def __init__(self, voltage):
+		'''Method sets initial parameter.'''
 		self.voltage = voltage
 		
 
 def totalresistance(RESISTORS_COUNT):
-	'''Function for calculating total resistance in circuit.
-		It create all resistors object and sum the resistance.'''
+	'''Function for calculating total resistance in circuit. It create all resistors object and sum the resistance.'''
 	resistance = []
 	for res in range(RESISTORS_COUNT):
 		while True:
@@ -34,7 +35,7 @@ def totalresistance(RESISTORS_COUNT):
 	return total_resistance
 	
 def totalvoltage(SOURCES_COUNT):
-	'''Function totalvoltage create source objects and sum its voltages.'''
+	'''Function creates source objects and sum its voltages.'''
 	sources = []
 	for sour in range(SOURCES_COUNT):
 		while True:
@@ -48,12 +49,12 @@ def totalvoltage(SOURCES_COUNT):
 	return total_voltage
 
 def current(U, R):
-	'''Function takes total_voltage and total_resistance and calculate total current. '''
+	'''Function takes total voltage and total resistance and calculates total current. '''
 	I = U / R
 	return I
 	
 def voltage_on_all_res(I, RESISTORS_COUNT):
-	'''Function takes list Resistors and call method voltage_on_resistor for each one. '''
+	'''Function takes list Resistors and calls method voltage_on_resistor for each of them. '''
 	UR = []
 	for R in range(RESISTORS_COUNT):
 		UR.append(RESISTORS[R].voltage_on_resistor(I))
@@ -62,7 +63,7 @@ def voltage_on_all_res(I, RESISTORS_COUNT):
 
 	
 def voltage_on_one_res(I, RESISTORS_COUNT):
-	'''Function takes position of object in list Resistor and for it method voltage_on_resistor. '''
+	'''Function takes position of one object in list Resistors and calls for this object method voltage_on_resistor.'''
 	while True:
 		try:
 			position = int(input('Index of resistor: ', ))
@@ -75,6 +76,7 @@ def voltage_on_one_res(I, RESISTORS_COUNT):
 			print('It was not a number. Please, try it again and better.')
 			
 def counts():
+	'''Function sets number of resistors and sources in circuit.'''
 	count = []
 	while True:
 		try:
